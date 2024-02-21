0daf27eab6c741012a9b6146fde8683bfe0e8e61
Muharrem Koroglu <muharrem-koroglu.99@hotmail.com>
2024-02-21 15:34:33 +0300

    Initial Commit

---
+++ ByteCoin/ByteCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByteCoinApp.swift
+//  ByteCoin
+//
+//  Created by Muharrem Köroğlu on 21.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ByteCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
